530f4b4e3467c090abfc89d73d1b9e03e76f9d88
Anthony Tran <anthonytran.dev@gmail.com>
2018-07-26 04:09:58 +0100

    Bug Fix allows user to pick game mode

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      selectedPreference: true
+      selectedPreference: false
     };
     this.selectPlayerOption = this.selectPlayerOption.bind(this);
   }
